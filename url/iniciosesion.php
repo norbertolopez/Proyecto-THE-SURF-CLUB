91aebde905eb451e062000f28bad62d60d9cfebe
delarosa <delarosa.management@gmail.com>
2016-02-24 11:11:24 +0100

    -a

--- url/iniciosesion.php
+++ url/iniciosesion.php
@@ -2,7 +2,13 @@
 
 session_start();
 
+?>
 
+<?php
+
+include_once("./db_configuration.php");
+
+?>
 
 include("encabezado.php");
 print "<LINK REL='stylesheet' TYPE='text/css' HREF='../css/estilos.css'>";
