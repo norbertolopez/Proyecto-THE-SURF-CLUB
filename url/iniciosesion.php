bbf795619889cc3c52a9fb1a95e656bd26c0e377
delarosa <delarosa.management@gmail.com>
2016-02-26 00:54:05 +0100

    -a

--- url/iniciosesion.php
+++ url/iniciosesion.php
@@ -2,7 +2,9 @@
 
 session_start();
 include("encabezado.php");
+
 include_once("db_configuration.php");
+
 print "<LINK REL='stylesheet' TYPE='text/css' HREF='../css/estilos.css'>";
 
 		
@@ -11,7 +13,7 @@ print "<LINK REL='stylesheet' TYPE='text/css' HREF='../css/estilos.css'>";
 				if (isset ($_REQUEST['usuariol']) && isset ($_REQUEST['contrasenal']))
 				{
 					// Conectar con el servidor de base de datos.
-      				$conexion = mysql_connect ($db_host,$db_user)
+      				$conexion = mysql_connect ($db_host,$db_user, "")
         				 or die ("No se puede conectar con el servidor");
 
    					// Seleccionar base de datos.
