919ab8e481ea7ff35a6d5ac8a4929aa384cf108f
delarosa <delarosa.management@gmail.com>
2016-02-26 11:19:46 +0100

    -a

--- url/nuevousuario2monitores.php
+++ url/nuevousuario2monitores.php
@@ -162,18 +162,18 @@ else
       			if(isset($_REQUEST['enviarl']) && $error=="")
    				{ 
    						// Conectar con el servidor de base de datos
-      			//		$conexion = mysql_connect ($db_host, $db_user, $db_password)
-        		//			 or die ("No se puede conectar con el servidor");
+      					$conexion2 = mysql_connect ($db_host, $db_user, $db_password)
+        					 or die ("No se puede conectar con el servidor");
 
    						// Seleccionar base de datos
-     			//		mysql_select_db ($db_name)
-         		//			or die ("No se puede seleccionar la base de datos");
+     					mysql_select_db ($db_name)
+         					or die ("No se puede seleccionar la base de datos");
 						$usuario2=$_SESSION['usuario'];
    						// Enviar consulta para insertar contacto en la agenda.
 						$instruccion = "insert into monitores (dni_monitor,nombre_monitor,apellidos_monitor,email_monitor,telefono_monitor,direccion_monitor,actividad_monitor,foto_monitor) values ('".$_REQUEST['dnil']."','".$_REQUEST['nombrel']."','".$_REQUEST['apellidol']."','".$_REQUEST['emaill']."','".$_REQUEST['telefonol']."','".$_REQUEST['direcionl']."','".$_REQUEST['actividadl']."','".$nombreFichero."')" ;
-						$consulta = mysql_query ($instruccion, $conexion)
+						$consulta = mysql_query ($instruccion, $conexion2)
         					 or die ("Fallo en la consultass");        					
-         				mysql_close ($conexion);
+         				mysql_close ($conexion2);
          				if ($copiarFichero)
        						  move_uploaded_file ($_FILES['imagenl']['tmp_name'],$nombreDirectorio . $nombreFichero);	
    					?>
